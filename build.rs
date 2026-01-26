39295eb5a695e9332909e4dae3c046f5abcffbc3
wwwzne <3466389208@qq.com>
2026-01-27 05:17:56 +0800

    update

--- build.rs
+++ build.rs
@@ -1,5 +1,23 @@
-fn main() {
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() -> std::io::Result<()> {
+    println!("cargo:warning={}","开始");
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let target_dir = out_dir.ancestors().nth(3).unwrap().to_path_buf();
+
+
+    fs::copy("shell/runner.cmd", target_dir.join("runner.cmd"))?;
+    fs::copy("shell/server.cmd", target_dir.join("server.cmd"))?;
+    fs::copy("shell/test.cmd", target_dir.join("test.cmd"))?;
+    fs::copy("shell/wwwzne.config.json", target_dir.join("wwwzne.config.json"))?;
+
     let mut res = winres::WindowsResource::new();
     res.set_icon("icon.ico");
-    res.compile().unwrap();
+    res.compile()?;
+
+    println!("cargo:warning=结束");
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,8 @@ static/*::作者字段*/AUTHOR: &str = r#"
   d888P         d888   Y8888b   d0888b
 d888888888888   d888    Y888b   d088888888889
 "#;
-static/*::配置文件*/CONFIG:&str="wwwzne.config.json";
-fn load_config_from_exe_parent(file: PathBuf) -> Result<HashMap<String, Vec<String>>> {
+static /*::配置文件*/CONFIG: &str = "wwwzne.config.json";
+fn load_config(file: PathBuf) -> Result<HashMap<String, Vec<String>>> {
     let f = File::open(file)?;
     let reader = BufReader::new(f);
     let v: Value = serde_json::from_reader(reader)?;
@@ -74,118 +74,145 @@ fn main() -> Result<()> {
         ).subcommand(
             Command::new("path").about("查看工具位置").long_about("详细查看工具存放路径")
         ).subcommand(
-            Command::new("wwwzne").about("含义").long_about("wwwzne的含义")
+            Command::new("wwwzne").about("logo释意").long_about("wwwzne的释意")
+        ).subcommand(
+            Command::new("global").about("查看全局指令").long_about("详细查看设置的全局指令")
+        ).subcommand(
+            Command::new("local").about("查看局部指令").long_about("详细查看设置的局部指令")
         ).arg(
             Arg::new("subcmd").index(1).help("子命令名称"),
         ).arg(
             Arg::new("args").index(2).trailing_var_arg(true).num_args(0..).help("传递给执行模板的额外参数"),
         );
+
     let matches = app.get_matches();
     match matches.subcommand() {
         Some(("version", _)) => {
-            println!("{}", env!("CARGO_PKG_VERSION"));
-            return Ok(());
+            print!(
+                "{} {}",
+                "版本号:".bright_red(),
+                env!("CARGO_PKG_VERSION").bright_red()
+            );
+            Ok(())
         }
         Some(("path", _)) => {
-            println!("{}", parent.to_string_lossy().bold().bright_green());
-            return Ok(());
+            print!("{}", parent.to_string_lossy().bold().bright_green());
+            Ok(())
         }
         Some(("wwwzne", _)) => {
             print!("{}", "World Wide Web Zend Node Element".bright_yellow());
-            return Ok(());
+            Ok(())
         }
         Some(("config", _)) => {
             if global_config.exists() {
                 println!(
                     "{} {}",
-                    "全局配置文件:".bold().bright_yellow(),
-                    local_config.to_string_lossy().bright_green()
+                    "全局配置文件:".bold().bright_green(),
+                    global_config.to_string_lossy().bright_green()
                 );
             } else {
                 println!(
                     "{} {}",
                     "全局配置文件:".bold().bright_yellow(),
-                    "未配置".bright_green()
+                    "未配置".bright_yellow()
                 );
             }
             if local_config.exists() {
                 println!(
                     "{} {}",
-                    "局部配置文件:".bold().bright_yellow(),
+                    "局部配置文件:".bold().bright_green(),
                     local_config.to_string_lossy().bright_green()
                 );
             } else {
                 println!(
                     "{} {}",
                     "局部配置文件:".bold().bright_yellow(),
-                    "未配置(可选)".bright_green()
+                    "未配置(可选)".bright_yellow()
                 );
             }
-            return Ok(());
+            Ok(())
         }
-        Some((_, _)) => {}
-        None => {}
-    }
-    let subcmd = matches.get_one::<String>("subcmd").map(|s| s.as_str());
-    if subcmd.is_none() {
-        print!("{}", AUTHOR.trim().bright_green().bold());
-        return Ok(());
-    }
-    let subcmd = subcmd.unwrap();
-    if global_config.exists() {
-        let cfg_2 = load_config_from_exe_parent(global_config)?;
-        let cfg_1 = if local_config.exists() {
-            Some(load_config_from_exe_parent(local_config)?)
-        } else {
-            None
-        };
-        let templates = match cfg_1
-            .as_ref()
-            .and_then(|c| c.get(subcmd))
-            .or_else(|| cfg_2.get(subcmd))
-        {
-            Some(t) => t,
-            None => {
-                println!("{} {}", "未知指令:".bold().bright_red(), subcmd);
-                return Ok(());
-            }
-        };
-        let extra = matches
-            .get_many::<String>("args")
-            .map(|vals| vals.map(|s| s.as_str()).collect::<Vec<_>>().join(" "))
-            .unwrap_or_default();
-        let mut script = format!(
-            "set wwwznepath={} & set wwwznedir={}",
-            parent.to_string_lossy(),
-            cwd.to_string_lossy()
-        );
-
-        for tpl in templates {
-            let part = if extra.is_empty() {
-                tpl.clone()
+        Some(("global", _)) => {
+            if global_config.exists() {
+                for (i, j) in load_config(global_config.clone())? {
+                    println!("{}: {}", i.bright_yellow(), j.first().unwrap().as_str())
+                }
             } else {
-                format!("{} {}", tpl, extra)
-            };
-            script.push_str(" & ");
-            script.push_str(&part);
+                println!("{}", "未设置".bright_yellow().bold());
+            }
+            Ok(())
         }
+        Some(("local", _)) => {
+            if local_config.exists() {
+                for (i, j) in load_config(local_config.clone())? {
+                    println!("{}: {}", i.bright_yellow(), j.first().unwrap().as_str())
+                }
+            } else {
+                println!("{}", "未设置".bright_yellow().bold());
+            }
+            Ok(())
+        }
+        Some((_, _)) => Ok(()),
+        None => {
+            let i = matches.get_one::<String>("subcmd").map(|s| s.as_str());
+            if i.is_none() {
+                print!("{}", AUTHOR.trim().bright_green().bold());
+            } else if global_config.exists() {
+                let cfg_2 = load_config(global_config)?;
+                let cfg_1 = if local_config.exists() {
+                    Some(load_config(local_config)?)
+                } else {
+                    None
+                };
+                let templates = match cfg_1
+                    .as_ref()
+                    .and_then(|c| c.get(i.unwrap()))
+                    .or_else(|| cfg_2.get(i.unwrap()))
+                {
+                    Some(t) => t,
+                    None => {
+                        print!("{} {}", "未知指令:".bold().bright_red(), i.unwrap());
+                        return Ok(());
+                    }
+                };
+                let extra = matches
+                    .get_many::<String>("args")
+                    .map(|vals| vals.map(|s| s.as_str()).collect::<Vec<_>>().join(" "))
+                    .unwrap_or_default();
+                let mut script = format!(
+                    "set wwwznepath={} & set wwwznedir={}",
+                    parent.to_string_lossy(),
+                    cwd.to_string_lossy()
+                );
 
-        //windows平台特有
-        #[cfg(target_os = "windows")]
-        {
-            let status = ProcCommand::new("cmd")
-                .arg("/V:ON")
-                .arg("/C")
-                .arg(&script)
-                .status()?;
-            println!("{}", status.to_string().on_bright_yellow().black());
-        }
-        //linux平台特有
-        #[cfg(target_os = "linux")]
-        {
-            let status = ProcCommand::new("sh").arg("-c").arg(&script).status()?;
-            println!("{}", status);
+                for tpl in templates.iter().skip(1) {
+                    let part = if extra.is_empty() {
+                        tpl.clone()
+                    } else {
+                        format!("{} {}", tpl, extra)
+                    };
+                    script.push_str(" & ");
+                    script.push_str(&part);
+                }
+
+                //windows平台特有
+                #[cfg(target_os = "windows")]
+                {
+                    let status = ProcCommand::new("cmd")
+                        .arg("/V:ON")
+                        .arg("/C")
+                        .arg(&script)
+                        .status()?;
+                    println!("{}", status.to_string().on_bright_yellow().black());
+                }
+                //linux平台特有
+                #[cfg(target_os = "linux")]
+                {
+                    let status = ProcCommand::new("sh").arg("-c").arg(&script).status()?;
+                    println!("{}", status);
+                }
+            }
+            Ok(())
         }
     }
-    Ok(())
 }
